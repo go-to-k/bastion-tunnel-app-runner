abd0fbaa0adf90e549008d52bef80b82b6968214
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-05-14 05:19:36 +0900

    update: using constructs

---
+++ lib/construct/app-runner.ts
@@ -0,0 +1,206 @@
+import { CustomResource, RemovalPolicy } from "aws-cdk-lib";
+import { Repository } from "aws-cdk-lib/aws-ecr";
+import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
+import { Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Provider } from "aws-cdk-lib/custom-resources";
+import { DockerImageName, ECRDeployment } from "cdk-ecr-deployment";
+import { Construct } from "constructs";
+import { resolve } from "path";
+import { ConfigStackProps, StackInput } from "../config";
+import { Cpu, Memory, Service, Source, VpcConnector } from "@aws-cdk/aws-apprunner-alpha";
+import { SecurityGroup, Subnet, Vpc } from "aws-cdk-lib/aws-ec2";
+import { CfnService } from "aws-cdk-lib/aws-apprunner";
+
+export class AppRunnerConstruct extends Construct {
+    private stackInput: StackInput;
+
+    constructor(scope: Construct, id: string, props: ConfigStackProps) {
+        super(scope, id);
+
+        this.stackInput = props.config;
+
+        /*
+          ECR
+         */
+        const repository = new Repository(this, "ImageRepository", {
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteImages: true,
+        });
+
+        repository.addLifecycleRule({
+            rulePriority: 1,
+            maxImageCount: 3,
+        });
+
+        const image = new DockerImageAsset(this, "AppRunnerImage", {
+            directory: resolve(__dirname, "../.."),
+            platform: Platform.LINUX_AMD64,
+        });
+
+        new ECRDeployment(this, "DeployImage", {
+            src: new DockerImageName(image.imageUri),
+            dest: new DockerImageName(
+                `${repository.repositoryUri}:latest`,
+            ),
+        });
+
+        /*
+  Custom Resource Lambda for creation of AutoScalingConfiguration
+ */
+        const customResourceLambda = new NodejsFunction(this, "custom", {
+            runtime: Runtime.NODEJS_16_X,
+            bundling: {
+                forceDockerBundling: false,
+            },
+            initialPolicy: [
+                new PolicyStatement({
+                    actions: ["apprunner:*AutoScalingConfiguration*"],
+                    resources: ["*"],
+                }),
+            ],
+        });
+
+        /*
+          AutoScalingConfiguration
+        */
+        const autoScalingConfigurationProvider = new Provider(
+            this,
+            "AutoScalingConfigurationProvider",
+            {
+                onEventHandler: customResourceLambda,
+            },
+        );
+
+        const autoScalingConfigurationProperties: { [key: string]: string } = {};
+        autoScalingConfigurationProperties["AutoScalingConfigurationName"] = this.stackInput.appName;
+        autoScalingConfigurationProperties["MaxConcurrency"] = String(
+            this.stackInput.autoScalingConfigurationArnProps.maxConcurrency,
+        );
+        autoScalingConfigurationProperties["MaxSize"] = String(
+            this.stackInput.autoScalingConfigurationArnProps.maxSize,
+        );
+        autoScalingConfigurationProperties["MinSize"] = String(
+            this.stackInput.autoScalingConfigurationArnProps.minSize,
+        );
+
+        const autoScalingConfiguration = new CustomResource(this, "AutoScalingConfiguration", {
+            resourceType: "Custom::AutoScalingConfiguration",
+            properties: autoScalingConfigurationProperties,
+            serviceToken: autoScalingConfigurationProvider.serviceToken,
+        });
+        const autoScalingConfigurationArn = autoScalingConfiguration.getAttString(
+            "AutoScalingConfigurationArn",
+        );
+
+        /*
+          AccessRole for AppRunner Service
+        */
+        const appRunnerAccessRole = new Role(this, "AppRunnerAccessRole", {
+            assumedBy: new ServicePrincipal("build.apprunner.amazonaws.com"),
+            managedPolicies: [
+                ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSAppRunnerServicePolicyForECRAccess"),
+            ],
+        });
+
+        /*
+          InstanceRole for AppRunner Service
+        */
+        const appRunnerInstanceRole = new Role(this, "AppRunnerInstanceRole", {
+            assumedBy: new ServicePrincipal("tasks.apprunner.amazonaws.com"),
+        });
+
+        appRunnerInstanceRole.addToPolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                "ssm:DeleteActivation",
+                "ssm:RemoveTagsFromResource",
+                "ssm:AddTagsToResource",
+                "ssm:CreateActivation",
+                "ssm:DeregisterManagedInstance",
+            ],
+            resources: ["*"],
+        }));
+
+        appRunnerInstanceRole.addToPolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                "ssm:PutParameter",
+                "ssm:GetParameter*",
+                "ssm:DescribeParameters",
+            ],
+            resources: ["*"],
+        }));
+
+        appRunnerInstanceRole.addToPolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                "iam:PassRole",
+            ],
+            resources: ["*"],
+            conditions: {
+                "StringEquals": {
+                    "iam:PassedToService": "ssm.amazonaws.com",
+                },
+            }
+        }));
+
+
+        /*
+          L2 Construct(alpha version) for VPC Connector
+        */
+        const vpc = Vpc.fromLookup(this, "VPCForSecurityGroupForVpcConnector", {
+            vpcId: this.stackInput.vpcConnectorProps.vpcID,
+        });
+
+        const securityGroupForVpcConnector = new SecurityGroup(
+            this,
+            "SecurityGroupForVpcConnector",
+            {
+                vpc: vpc,
+                description: "for AppRunner VPC Connector",
+            },
+        );
+
+        const vpcConnector = new VpcConnector(this, "VpcConnector", {
+            vpc: vpc,
+            securityGroups: [securityGroupForVpcConnector],
+            vpcSubnets: {
+                subnets: [
+                    Subnet.fromSubnetId(this, "Subnet1", this.stackInput.vpcConnectorProps.subnetID1),
+                    Subnet.fromSubnetId(this, "Subnet2", this.stackInput.vpcConnectorProps.subnetID2),
+                ],
+            },
+        });
+
+        /*
+          L2 Construct(alpha version) for AppRunner Service
+        */
+        const appRunnerService = new Service(this, "AppRunnerService", {
+            accessRole: appRunnerAccessRole,
+            instanceRole: appRunnerInstanceRole,
+            source: Source.fromEcr({
+                imageConfiguration: {
+                    environment: {
+                        ["APP_NAME"]: this.stackInput.appName,
+                        ["PORT"]: String(this.stackInput.sourceConfigurationProps.port),
+                    },
+                    port: this.stackInput.sourceConfigurationProps.port,
+                },
+                repository: repository,
+            }),
+            cpu: Cpu.of(this.stackInput.instanceConfigurationProps.cpu),
+            memory: Memory.of(this.stackInput.instanceConfigurationProps.memory),
+            vpcConnector: vpcConnector,
+        });
+
+        const cfnAppRunner = appRunnerService.node.defaultChild as CfnService;
+        cfnAppRunner.autoScalingConfigurationArn = autoScalingConfigurationArn;
+        cfnAppRunner.healthCheckConfiguration = {
+            path: "/",
+            protocol: "HTTP",
+        };
+        cfnAppRunner.addPropertyOverride("SourceConfiguration.AutoDeploymentsEnabled", true);
+    }
+}
---
+++ lib/construct/ssm.ts
@@ -0,0 +1,40 @@
+import { Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import { Construct } from "constructs";
+import { ConfigStackProps, StackInput } from "../config";
+
+export class SSMConstruct extends Construct {
+    private stackInput: StackInput;
+
+    constructor(scope: Construct, id: string, props: ConfigStackProps) {
+        super(scope, id);
+
+        this.stackInput = props.config;
+
+        /* 
+        SSM Parameter Store for Managed Instance ID
+        */
+        new StringParameter(this, "ManagedInstanceIDParameter", {
+            parameterName: `/ManagedInstanceIDParameter/${this.stackInput.appName}`,
+            stringValue: "ManagedInstanceIDParameter",
+            description: "Managed Instance ID Parameter",
+        })
+
+        /* 
+          SSM Service Role for container
+         */
+        const ssmServiceRole = new Role(this, "SSMServiceRole", {
+            roleName: `${this.stackInput.appName}-SSMServiceRole`,
+            assumedBy: new ServicePrincipal("ssm.amazonaws.com"),
+            managedPolicies: [
+                ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"),
+            ],
+        })
+
+        ssmServiceRole.addToPolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ["ssm:DeregisterManagedInstance"],
+            resources: ["*"],
+        }));
+    }
+}
